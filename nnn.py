d6aa929f9951e60872e1367f80e618eb9a872a9c
RafalKornel <rafal.kornel123@gmail.com>
2020-11-02 00:23:37 +0100

    [MTN] Changed config

--- nnn.py
+++ nnn.py
@@ -2,7 +2,7 @@ from app import create_app, db
 from app.models import User
 from flask_migrate import Migrate
 
-app = create_app("development")
+app = create_app("production")
 
 migrate = Migrate(app, db)
 
