c74802361aa3b0d9881eac46f04ea827170b9d2c
RafalKornel <rafal.kornel123@gmail.com>
2020-11-02 00:44:29 +0100

    [FIX] Fixed database creation commands

--- nnn.py
+++ nnn.py
@@ -9,3 +9,11 @@ migrate = Migrate(app, db)
 @app.shell_context_processor
 def make_shell_context():
     return { "db":db, "User":User }
+
+@app.cli.command("create_tables")
+def create_tables():
+    db.create_all()
+
+@app.cli.command("drop_tables")
+def drop_tables():
+    db.drop_all()
