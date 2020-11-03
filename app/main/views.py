ea9fe7be01f8a7ee2d547c06e029a8525c316245
RafalKornel <rafal.kornel123@gmail.com>
2020-11-03 22:12:29 +0100

    [NEW] Finished layout

--- app/main/views.py
+++ app/main/views.py
@@ -10,7 +10,7 @@ from ..models import User
 @main.route("/")
 def index():
     if not current_user.is_authenticated:
-        return render_template("index.html")
+        return redirect(url_for("auth.login"))
     else: 
         if not current_user.seen_today():
             current_user.answered_today = False
@@ -39,33 +39,42 @@ def calendar():
         calendar = users_calendar)
 
 
-@main.route("/question", methods=["GET", "POST"])
+@main.route("/question")
 @login_required
 def question():
 
     if current_user.answered_today: 
-        flash("Already answered today.")
         return redirect(url_for("main.calendar"))
 
     if current_user.failed:
-        flash("Sorry, you failed.")
         return redirect(url_for("main.calendar"))
+
+    return render_template( "question.html", form=QuestionForm() )
+
+
+@main.route("/answer/<ans>")
+@login_required
+def answer(ans):
+
+    if ans not in ["yes", "no"]:
+        return redirect(url_for("main.question"))
+
+    answer = (ans == "yes")
+
+    current_user.date_of_failure = date.today()
+    current_user.failed = answer
+    current_user.answered_today = True
+    current_user.last_seen = date.today()
+    
+    db.session.add(current_user)
+    db.session.commit()
+    
+    return redirect(url_for("main.calendar"))
+
 
     form = QuestionForm()
     if form.validate_on_submit():
         answer = True if form.answer.data == "yes" else False
 
-        if answer:
-            current_user.date_of_failure = date.today()
-
-        current_user.failed = answer
-        current_user.answered_today = True
-        current_user.last_seen = date.today()
-
-        db.session.add(current_user)
-        db.session.commit()
-
-        return redirect(url_for("main.calendar"))
-
-
-    return render_template( "question.html", form=form )
+        
+    return redirect(url_for("main.question"))
